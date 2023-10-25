d7c05ae48554a1a7996a5595db9499430b7f350e
naverolek2 <sebaskiba11@gmail.com>
2023-10-25 14:44:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require('mongodb');
+const mongoString = "mongodb+srv://sebaskiba11:1234@pierwszadbmongo.kywosci.mongodb.net/?retryWrites=true&w=majority"
+
+const client = new MongoClient(mongoString);
+let maximumNumberOfResults = Number.MAX_SAFE_INTEGER
+
+async function main() {
+
+    try {
+        await client.connect();
+        await listDB(client);
+    } catch(error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDB(client) {
+    const myDB = client.db("sample_airbnb").collection("listingsAndReviews")
+    cursor = myDB.find(
+        {
+            bedrooms : 1
+        }
+    ).limit(maximumNumberOfResults);
+    const results = await cursor.toArray();
+    results.forEach((result, i) => {
+        date = new Date(result.last_review).toDateString();
+        console.log(`_id: ${result.name}`);
+
+    })
+
+    
+    
+    
+
+
+
+    
+}
+
+main();
